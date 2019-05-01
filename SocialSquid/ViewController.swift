770b2c3903d8501b95bf94d7dd8d89f06bde9b5c
Colton Steeve <coltonsteeve@gmail.com>
2019-05-01 00:57:50 -0500

    Added alert when Google button is pressed.

--- SocialSquid/ViewController.swift
+++ SocialSquid/ViewController.swift
@@ -14,7 +14,18 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        print("This is a Git tutorial")
+    }
 
-
+    @IBAction func showAlert(_ sender: Any) {
+        let alertController = UIAlertController(title: "iOScreator", message:
+            "Hello, world!", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
 }
 
